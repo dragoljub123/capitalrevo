f3ca63aa189fdaf0ffc42d6b1c9bd6773c6a7c84
dragoljub123 <dragoljub.miladinovic@bazarko.rs>
2024-08-14 13:40:55 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/cr.ico" />
+      </head>
       <body className={inter.className}>
         {children}
         <FooterHome />
