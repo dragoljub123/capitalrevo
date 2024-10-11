666eea9bf423606f85f919fc61fd10268dc4a7c2
Anastasija <anastasija.miladinovic@gmail.com>
2024-10-11 09:13:12 +0200

    Updated Leverage

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -147,11 +147,9 @@ export default async function Accounts() {
               {/* Leverage */}
               <tr>
                 <td className="px-6 py-3 text-sm font-semibold">Leverage</td>
-                <td className="px-6 py-3 text-sm">N/A</td>
-                <td className="px-6 py-3 text-sm">N/A</td>
-                <td className="px-6 py-3 text-sm plavigradijent font-semibold">
-                  1:200
-                </td>
+                <td className="px-6 py-3 text-sm">1:30</td> {/* Updated for Basic Account */}
+                <td className="px-6 py-3 text-sm">1:100</td> {/* Updated for Silver Account */}
+                <td className="px-6 py-3 text-sm plavigradijent font-semibold">1:200</td>
                 <td className="px-6 py-3 text-sm">1:300</td>
                 <td className="px-6 py-3 text-sm">1:400</td>
               </tr>
